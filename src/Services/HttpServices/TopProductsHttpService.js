6c286f25bc657ade24ad9ceb2e3b841afd947a0c
Mariam Hayrapetyan <mariamsaghatelyan6@gmail.com>
2025-01-26 11:17:01 +0400

    Fix review comment

--- src/Services/HttpServices/TopProductsHttpService.js
+++ src/Services/HttpServices/TopProductsHttpService.js
@@ -5,6 +5,7 @@ import { environment } from "../../environment.dev.js";
 export const getTopProductById = async() => {
     try {
         const response = await httpClient.get(`product/top-product/${environment.appId}`);
+        console.log(response.data)
         return response;
     } catch (error) {
         console.error('Error fetching product:', error);
