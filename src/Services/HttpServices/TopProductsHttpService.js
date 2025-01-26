3e7d3958a245bce05dd93135c67b9b7924260c8f
Hovhannes Khachibabyan <hovo@gmail.com>
2025-01-26 17:01:07 +0400

    Refactor and clean up TopProductsHttpService and Banner

--- src/Services/HttpServices/TopProductsHttpService.js
+++ src/Services/HttpServices/TopProductsHttpService.js
@@ -1,14 +1,12 @@
 import {httpClient} from "../HttpClientService.js";
-import { environment } from "../../environment.dev.js";
+import {environment} from "../../environment.dev.js";
 
 
-export const getTopProductById = async() => {
-    try {
-        const response = await httpClient.get(`product/top-product/${environment.appId}`);
-        console.log(response.data)
-        return response;
-    } catch (error) {
-        console.error('Error fetching product:', error);
-    }
+export const getTopProductById = async () => {
+  try {
+    return await httpClient.get(`product/top-product/${environment.appId}`);
+  } catch (error) {
+    console.error('Error fetching product:', error);
+  }
 }
 
