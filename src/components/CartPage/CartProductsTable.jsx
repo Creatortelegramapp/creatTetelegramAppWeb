18daaaca91c56a5ac321a4a040749ce0cb1ac9d6
hayk <haykpoghosyan002@gmail.com>
2025-03-05 19:59:47 +0400

    bug fis:add to cart button in single Product

--- src/components/CartPage/CartProductsTable.jsx
+++ src/components/CartPage/CartProductsTable.jsx
@@ -1,7 +1,7 @@
 import ProductsTable from "../../shared/components/ProductsTable.jsx";
 import {useCartProducts} from "../../hooks/useCartProducts.jsx";
 
-export default function CartProductsTable({ className,isCart }) {
+export default function CartProductsTable({isCart }) {
   const { cartProducts, removeProductById } = useCartProducts();
 
   const onRemove = (id) => {
