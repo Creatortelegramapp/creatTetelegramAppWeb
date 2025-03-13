d3098986d4551a874130efc3c9d53e34d4e29522
Mariam Hayrapetyan <mariamsaghatelyan6@gmail.com>
2025-03-13 17:37:17 +0400

    fixed bugs in price range slider

--- src/components/PriceRangeSlider.jsx
+++ src/components/PriceRangeSlider.jsx
@@ -1,43 +1,53 @@
 import { useEffect, useState } from "react";
 
-const PriceRangeInput = ({ min, max, onChange, initialValues }) => {
+const PriceRangeInput = ({ min, onChange, initialValues }) => {
     const [values, setValues] = useState(initialValues);
+    const [delayRequest, setDelayRequest] = useState(initialValues);
 
     useEffect(() => {
         setValues(initialValues);
     }, [initialValues]);
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (delayRequest[0] !== "" && delayRequest[1] !== "") {
+                onChange(delayRequest);
+            }
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, [delayRequest, onChange]);
+
     const handleChange = (e, index) => {
         const newValue = e.target.value === "" ? "" : Number(e.target.value);
         const newValues = [...values];
         newValues[index] = newValue;
+
         if (newValue !== "") {
             if (newValues[0] < min) newValues[0] = min;
-            if (newValues[1] > max) newValues[1] = max;
             if (newValues[0] > newValues[1]) newValues[0] = newValues[1];
         }
+
         setValues(newValues);
-        if (newValue !== "") onChange(newValues);
+        setDelayRequest(newValues);
     };
 
     return (
-        <div className="w-full p-4 flex items-center gap-4">
+        <div className="w-full flex items-center gap-4">
             <input
                 type="number"
-                className="border p-2 rounded w-24"
+                className="border p-2 rounded-lg w-24 focus:outline-none"
                 value={values[0] === "" ? "" : values[0]}
                 onChange={(e) => handleChange(e, 0)}
                 min={min}
-                max={values[1]}
             />
             <span className="text-lg font-semibold">-</span>
             <input
                 type="number"
-                className="border p-2 rounded w-24"
+                className="border p-2 rounded-lg w-24 focus:outline-none"
                 value={values[1] === "" ? "" : values[1]}
-                onChange={(e) => handleChange(e, 1)}
+                onChange={(e) => handleChange(e, 1 )}
                 min={values[0]}
-                max={max}
             />
         </div>
     );
