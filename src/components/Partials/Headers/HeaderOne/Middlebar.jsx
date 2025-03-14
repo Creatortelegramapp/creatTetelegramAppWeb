04641d1adce2f900950ece2b943b44681d7e9694
hayk <haykpoghosyan002@gmail.com>
2025-03-14 11:12:38 +0400

    There was an issue related to the wishlist count.

--- src/components/Partials/Headers/HeaderOne/Middlebar.jsx
+++ src/components/Partials/Headers/HeaderOne/Middlebar.jsx
@@ -4,8 +4,8 @@ import ThinLove from "../../../Helpers/icons/ThinLove";
 import ThinPeople from "../../../Helpers/icons/ThinPeople";
 import SearchBox from "../../../Helpers/SearchBox";
 import { Link } from "react-router-dom";
-import {useCartProducts} from "../../../../hooks/useCartProducts.jsx";
-import {useEffect, useState} from "react";
+import { useCartProducts } from "../../../../hooks/useCartProducts.jsx";
+import { useEffect, useState } from "react";
 
 export default function Middlebar({ className, type }) {
   const isLoggedIn = !!localStorage.getItem("access_token");
@@ -15,117 +15,105 @@ export default function Middlebar({ className, type }) {
   const [wishlistCount, setWishlistCount] = useState(0);
 
   const updateWishlistCount = () => {
-    const wishlistItems = JSON.parse(localStorage.getItem("wishlist")) || [];
+    const wishlistItems = JSON.parse(localStorage.getItem("wishlist") || "[]");
     setWishlistCount(wishlistItems.length);
   };
 
   useEffect(() => {
     updateWishlistCount();
-
-    const handleWishlistUpdate = () => {
-      updateWishlistCount();
-    };
-
     window.addEventListener("storage", updateWishlistCount);
-    window.addEventListener("wishlistUpdated", handleWishlistUpdate);
+    window.addEventListener("wishlist-updated", updateWishlistCount);
 
     return () => {
       window.removeEventListener("storage", updateWishlistCount);
-      window.removeEventListener("wishlistUpdated", handleWishlistUpdate);
+      window.removeEventListener("wishlist-updated", updateWishlistCount);
     };
   }, []);
 
   useEffect(() => {
-    setCartProcuctQuantity(cartProducts.reduce((acc, current) => {
-      return acc + current.quantity
-    }, 0));
+    setCartProcuctQuantity(
+        cartProducts.reduce((acc, current) => acc + current.quantity, 0)
+    );
   }, [cartProducts]);
 
   return (
-    <div className={`w-full h-[86px] bg-white ${className}`}>
-      <div className="container-x mx-auto h-full">
-        <div className="relative h-full">
-          <div className="flex justify-between items-center h-full">
-            <div>
-              {type === 3 ? (
-                <Link to="/">
-                  <img
-                    width="152"
-                    height="36"
-                    src={`${
-                      import.meta.env.VITE_PUBLIC_URL
-                    }/assets/images/logo-3.svg`}
-                    alt="logo"
-                  />
-                </Link>
-              ) : type === 4 ? (
-                <Link to="/">
-                  <img
-                    width="152"
-                    height="36"
-                    src={`${
-                      import.meta.env.VITE_PUBLIC_URL
-                    }/assets/images/logo-4.svg`}
-                    alt="logo"
-                  />
-                </Link>
-              ) : (
-                <Link to="/">
-                  <img
-                    width="152"
-                    height="36"
-                    src={`${
-                      import.meta.env.VITE_PUBLIC_URL
-                    }/assets/images/logo.svg`}
-                    alt="logo"
-                  />
-                </Link>
-              )}
-            </div>
-            <div className="w-[517px] h-[44px]">
-              <SearchBox type={type} className="search-com" />
-            </div>
-            <div className="flex space-x-6 items-center">
-              <div className="favorite relative">
-                <Link to="/wishlist" className="relative">
-                  <ThinLove />
-                  {wishlistCount > 0 && (
-                      <span className="w-[18px] h-[18px] rounded-full bg-qh4-pink absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px] text-qblack">
-                        {wishlistCount}
-                      </span>
-                  )}
-                </Link>
+      <div className={`w-full h-[86px] bg-white ${className}`}>
+        <div className="container-x mx-auto h-full">
+          <div className="relative h-full">
+            <div className="flex justify-between items-center h-full">
+              <div>
+                {type === 3 ? (
+                    <Link to="/">
+                      <img
+                          width="152"
+                          height="36"
+                          src={`${import.meta.env.VITE_PUBLIC_URL}/assets/images/logo-3.svg`}
+                          alt="logo"
+                      />
+                    </Link>
+                ) : type === 4 ? (
+                    <Link to="/">
+                      <img
+                          width="152"
+                          height="36"
+                          src={`${import.meta.env.VITE_PUBLIC_URL}/assets/images/logo-4.svg`}
+                          alt="logo"
+                      />
+                    </Link>
+                ) : (
+                    <Link to="/">
+                      <img
+                          width="152"
+                          height="36"
+                          src={`${import.meta.env.VITE_PUBLIC_URL}/assets/images/logo.svg`}
+                          alt="logo"
+                      />
+                    </Link>
+                )}
               </div>
-              <div className="cart-wrapper group relative py-4">
-                <div className="cart relative cursor-pointer">
-                  <Link to="/cart">
-                    <span>
-                      <ThinBag/>
+              <div className="w-[517px] h-[44px]">
+                <SearchBox type={type} className="search-com" />
+              </div>
+              <div className="flex space-x-6 items-center">
+                <div className="favorite relative">
+                  <Link to="/wishlist" className="relative">
+                    <ThinLove />
+                    {wishlistCount > 0 && (
+                        <span className="w-[18px] h-[18px] rounded-full bg-qh4-pink absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px] text-qblack">
+                      {wishlistCount}
                     </span>
-                    {cartProcuctQuantity > 0 && (
-                      <span
-                           className="w-[18px] h-[18px] rounded-full bg-qh4-pink absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px] text-qblack">
-                           {cartProcuctQuantity}
-                      </span>
                     )}
                   </Link>
                 </div>
-                <Cart
-                    type={type}
-                    className="absolute -right-[45px] top-11 z-50 hidden group-hover:block"
-                />
-              </div>
-              <div>
-                <Link to={isLoggedIn ? "/profile" : "/login"}>
+                <div className="cart-wrapper group relative py-4">
+                  <div className="cart relative cursor-pointer">
+                    <Link to="/cart">
+                    <span>
+                      <ThinBag />
+                    </span>
+                      {cartProcuctQuantity > 0 && (
+                          <span className="w-[18px] h-[18px] rounded-full bg-qh4-pink absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px] text-qblack">
+                        {cartProcuctQuantity}
+                      </span>
+                      )}
+                    </Link>
+                  </div>
+                  <Cart
+                      type={type}
+                      className="absolute -right-[45px] top-11 z-50 hidden group-hover:block"
+                  />
+                </div>
+                <div>
+                  <Link to={isLoggedIn ? "/profile" : "/login"}>
                   <span>
                     <ThinPeople />
                   </span>
-                </Link>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
--- src/components/Wishlist/AddWishListButton.jsx
+++ src/components/Wishlist/AddWishListButton.jsx
@@ -1,27 +1,20 @@
-import   {useEffect,useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function AddWishListButton({ productId, wishlist, updateWishlist }) {
     const [isInWishlist, setIsInWishlist] = useState(false);
+
     useEffect(() => {
         setIsInWishlist(wishlist.includes(productId));
     }, [wishlist, productId]);
 
     const toggleWishlist = (e) => {
         e.preventDefault();
-        let newWishlist = isInWishlist
-            ? wishlist.filter(id => id !== productId)
+        const newWishlist = isInWishlist
+            ? wishlist.filter((id) => id !== productId)
             : [...wishlist, productId];
-
 
         setIsInWishlist(!isInWishlist);
         updateWishlist(newWishlist);
-
-        try {
-            localStorage.setItem("wishlist", JSON.stringify(newWishlist));
-            window.dispatchEvent(new Event("storage"));
-        } catch (error) {
-            console.error("error", error);
-        }
     };
 
     return (
@@ -29,8 +22,11 @@ export default function AddWishListButton({ productId, wishlist, updateWishlist 
             onClick={(e) => toggleWishlist(e)}
             className="appearance-none bg-transparent border-none p-0 m-0"
         >
-            <div className={`w-10 h-10 flex justify-center items-center rounded cursor-pointer 
-                ${isInWishlist ? "bg-purple-200 text-white" : "bg-white"}`}>
+            <div
+                className={`w-10 h-10 flex justify-center items-center rounded cursor-pointer ${
+                    isInWishlist ? "bg-purple-200 text-white" : "bg-white"
+                }`}
+            >
                 {isInWishlist ? "❤️" : "🤍"}
             </div>
         </button>
